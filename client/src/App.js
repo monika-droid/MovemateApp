b1a0750d61ff9f59239f5602f5ec4f61cf8bd919
monika410 <pmonika213@gmail.com>
2024-11-03 18:24:40 -0500

    changed app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,28 @@
 // src/App.js
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './Context/AuthContext';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import CustomerHome from './pages/CustomerHome';
 import MoversDashboard from './pages/MoversDashboard';
-import ProtectedRoute from './Context/ProtectedRoute';
 
 function App() {
   return (
-    <AuthProvider>
+    
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route
           path="/user"
           element={
-            <ProtectedRoute allowedRoles={['User']}>
-              <CustomerHome />
-            </ProtectedRoute>
-          }
+           
+              <CustomerHome />          }
         />
         <Route
           path="/mover"
           element={ <MoversDashboard /> }
         />
       </Routes>
-    </AuthProvider>
   );
 }
 
