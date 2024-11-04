a98210ecb03bad9840806f074aedb5d1bfac48a8
sarahm-30 <100020901+sarahm-30@users.noreply.github.com>
2024-11-03 21:43:45 -0500

    Login-register UI

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import CustomerHome from './pages/CustomerHome';
 import MoversDashboard from './pages/MoversDashboard';
-
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<CustomerHome />} />
       <Route path="/movers" element={<MoversDashboard />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
 
     </Routes>
   );
