d1d1d65bacda573cedc3a044ec3245d18e0873f4
monika410 <pmonika213@gmail.com>
2024-11-05 13:18:46 -0500

    comments

--- movemate_b/controller/VehicleController.js
+++ movemate_b/controller/VehicleController.js
@@ -35,10 +35,10 @@ const addVehicle = async (req, res) => {
 
 const getVehiclesByEmail = async (req, res) => {
   console.log("Inside getVehiclesByEmail function");
-  const { email } = req.params; // Assuming email is passed as a URL parameter
+  const { email } = req.params; 
 
   try {
-    const vehicles = await Vehicle.find({ mover_id: email }); // Fetch vehicles by mover_id
+    const vehicles = await Vehicle.find({ mover_id: email });
 
     if (!vehicles.length) {
       return res.status(200).json({ data: null, message: "No vehicles found for this email." });
